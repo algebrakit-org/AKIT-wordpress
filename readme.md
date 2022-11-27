af5ed1a0b09c266dc86326f460a0d26b29d433ea
algebrakit <martijn.slob@algebrakit.nl>
2022-11-27 20:41:33 +0100

    update akit theme

--- readme.md
+++ readme.md
@@ -2,10 +2,10 @@
 
 A Plugin to enable AlgebraKiT exercises on WordPress websites.
 
-Installation instructions for this plugin can be found here: (https://docs.algebrakit-learning.com/plugins/wordpress/)
+Installation instructions for this plugin can be found here: (https://docs.algebrakit.com/plugins/wordpress/)
 
 ---
 
 **AlgebraKiT**<br>
-Official Website: (https://algebrakit-learning.com)<br>
-Documentation: (https://docs.algebrakit-learning.com)
+Official Website: (https://algebrakit.com)<br>
+Documentation: (https://docs.algebrakit.com)
--- widgetLoader.js
+++ widgetLoader.js
@@ -6,7 +6,18 @@ function loadJs() {
 if (typeof(AlgebraKIT) == 'undefined') {
     var AlgebraKIT = {
         config: {
-            theme: '${THEME}'
+            theme: '${THEME}',
+            styles: {
+                general: {
+                    'border-radius': '10px'
+                },
+                multistep: {
+                    'color-worksheet-bg': 'white',
+                    'border-worksheet': '1px solid #ccc',
+                    'color-buttons-bg': 'white',
+                    'color-buttons-bg-hover': '#ccc',
+                }
+            }
         }
     };
     loadJs();
